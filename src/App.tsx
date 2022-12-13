16081b383e243715afa6429af209f2a779574eac
Jess2410 <jess2410@hotmail.fr>
2022-12-13 10:54:21 +0100

    Initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FunctionComponent, useState } from "react";
+import "./App.css";
+import Pokemon from "./models/pokemon";
+import POKEMONS from "./models/data-pokemons";
+// const name: String = "React";
 
-function App() {
+const App: FunctionComponent = () => {
+  const [pokemons] = useState<Pokemon[]>(POKEMONS);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokédex</h1>
+      <h4>Il y a {pokemons.length} pokémons dans le Pokédex !</h4>
     </div>
   );
-}
+};
 
 export default App;
