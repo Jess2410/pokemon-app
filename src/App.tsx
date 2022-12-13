d2a3299ffbb50de58a2d7fed4f38789b7650e876
Jess2410 <jess2410@hotmail.fr>
2022-12-13 12:23:02 +0100

    hooks personalised (formatDate + typeColor)

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import React, { FunctionComponent, useState } from "react";
+import React, { FunctionComponent } from "react";
 import "./App.css";
-import Pokemon from "./models/pokemon";
-import POKEMONS from "./models/data-pokemons";
+
+import PokemonList from "./pages/pokemon-list";
 // const name: String = "React";
 
 const App: FunctionComponent = () => {
-  const [pokemons] = useState<Pokemon[]>(POKEMONS);
-
-  return (
-    <div className="App">
-      <h1>Pokédex</h1>
-      <h4>Il y a {pokemons.length} pokémons dans le Pokédex !</h4>
-    </div>
-  );
+  return <PokemonList />;
 };
 
 export default App;
