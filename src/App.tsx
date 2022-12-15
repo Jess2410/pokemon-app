217c3d3052831693ed68fe6d995868b45eaf33d9
Jess2410 <jess2410@hotmail.fr>
2022-12-15 15:30:15 +0100

    Authenfication + Security

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React, { FunctionComponent } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
+import Login from "./pages/login";
 import PageNotFound from "./pages/page-not-found";
 import PokemonAdd from "./pages/pokemon-add";
 import PokemonsDetail from "./pages/pokemon-detail";
 import PokemonEdit from "./pages/pokemon-edit";
 import PokemonList from "./pages/pokemon-list";
+import PrivateRoute from "../src/PrivateRoute";
 
 const App: FunctionComponent = () => {
   return (
@@ -18,12 +20,16 @@ const App: FunctionComponent = () => {
         </div>
       </nav>
       <Routes>
-        <Route path="/" element={<PokemonList />} />
-        <Route path="/pokemons" element={<PokemonList />} />
-        <Route path="/pokemon/add" element={<PokemonAdd />} />
-        <Route path="/pokemons/:id" element={<PokemonsDetail />} />
-        <Route path="/pokemons/edit/:id" element={<PokemonEdit />} />
         <Route path="*" element={<PageNotFound />} />
+        <Route path="/login" element={<Login />} />
+
+        <Route element={<PrivateRoute />}>
+          <Route path="/" element={<PokemonList />} />
+          <Route path="/pokemons" element={<PokemonList />} />
+          <Route path="pokemons/add" element={<PokemonAdd />} />
+          <Route path="/pokemons/:id" element={<PokemonsDetail />} />
+          <Route path="pokemons/edit/:id" element={<PokemonEdit />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
